573bc3ac31d4b4b6cbf72ecb2dd6e216f2a54d87
piyush171 <70268673+piyush171@users.noreply.github.com>
2021-02-14 00:37:23 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -2,18 +2,18 @@
 $Name=$_POST['Name'];
 $Email=$_POST['Email'];
 $Phone=$_POST['Phone'];
-$Description=$_POST['Password'];
+$Message=$_POST['Message'];
 
 $con=mysqli_connect("localhost","root");
 
-mysqli_select_db($con,"pokemon");
+mysqli_select_db($con,"contact");
 
-$q="insert into Pokemon(Name,Email,Phone,Password)values('$Name','$Email','$Password','$Phone')";
+$q="insert into piyush(Name,Email,Phone,Message)values('$Name','$Email','$Phone','$Message')";
 
 $result=mysqli_query($con,$q);
 
 if($result)
 {
-	echo "Your enquiry will be finished we will contact you soon";
+	echo "Your enquiry will be finished we will contact you soon.Thank you for giving us time !!!!";
 }
 ?>
